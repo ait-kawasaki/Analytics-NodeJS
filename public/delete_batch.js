d22f170d76f9452ad7f7133b28f199c67db50a05
hjm-kawasaki <hjm.kawasaki@gmail.com>
2017-02-16 13:08:25 +0900

    test012

--- public/delete_batch.js
+++ public/delete_batch.js
@@ -13,6 +13,7 @@ function delete_batch(){
 
 	xhr.open("DELETE", "https://ibm-watson-ml.mybluemix.net/pm/v1/jobs/job03?accesskey=N4LnXREVJ44YgZy4MCnIJaU7NCAxoihZBHpVqS6abjaubA3inBFJ%2FeM4ZBhB2AxLHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi%20Et7TSlwLngbf%203XI66NrGzoVlIkEPrfJsLi9tdLYT4J2hgHmZgw6ZHGfMarLtgzVM%3D");
 	xhr.setRequestHeader("cache-control", "no-cache");
+	xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
 //	xhr.setRequestHeader("postman-token", "ee5c1a35-4477-dfd0-f15c-1a06674902d2");
 
 	xhr.send(data);
