0fd73192eb35d0bd51609dd7739cd476dc6630fe
hjm-kawasaki <hjm.kawasaki@gmail.com>
2017-02-16 14:38:29 +0900

    test014

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var cors = require('cors');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -14,6 +15,7 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var app = express();
+app.use(cors());
 app.use(function(request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -26,6 +28,34 @@ app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
+//app.post("/", function(request, response) {
+//    var options = {
+//        username: "replace",
+//        password: "replace"
+//    };
+//
+//    var url = "https://brsv2-6855bc66.ng.bluemix.net/DecisionService/rest" + "/vacationsRuleApp/1.0/vacationsRuleProject/json";
+//
+//    restler.postJson(url, request.body, options).on('complete', function(data) {
+//        response.send(data);
+//    });
+//});
+//
+//
+//app.get('/', function (req, res) {
+//  var data = {
+//    "bestAnimals": [
+//      "wombat",
+//      "corgi",
+//      "puffer fish",
+//      "owl",
+//      "crow"
+//    ]
+//  };
+//
+//  res.json(data);
+//});
+
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
--- public/batch.js
+++ public/batch.js
@@ -1,18 +1,20 @@
 function batch(){
-	var settings = {
-			  "async": true,
-			  "crossDomain": true,
-			  "url": "https://ibm-watson-ml.mybluemix.net/pm/v1/jobs/job03?accesskey=N4LnXREVJ44YgZy4MCnIJaU7NCAxoihZBHpVqS6abjaubA3inBFJ/eM4ZBhB2AxLHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi+Et7TSlwLngbf+3XI66NrGzoVlIkEPrfJsLi9tdLYT4J2hgHmZgw6ZHGfMarLtgzVM=",
-			  "method": "POST",
-			  "headers": {
-			    "content-type": "application/json;charset=UTF-8",
-			    "cache-control": "no-cache",
-			    "postman-token": "8aedbb60-a94d-5b7d-1108-5d246ed5715b"
-			  },
-			  "data": "{\r\n    \"action\": \"BATCH_SCORE\",\r\n    \"model\": {\r\n        \"id\": \"batch1\",\r\n        \"name\": \"attract_new_en2.str\"\r\n    },\r\n    \"dbDefinitions\": {\r\n        \"db\": {\r\n            \"type\": \"PostgreSQL\",\r\n            \"host\": \"echo-01.db.elephantsql.com\",\r\n            \"port\": 5432,\r\n            \"db\": \"bgurrocc\",\r\n            \"username\": \"bgurrocc\",\r\n            \"password\": \"8n_gJj3qo5bmIB4oPQUAtfvVRW93jv0e\",\r\n            \"options\": \"\"\r\n        }\r\n    },\r\n    \"setting\": {\r\n        \"inputs\": [{\r\n            \"odbc\": {\r\n                \"dbRef\": \"db\",\r\n                \"table\": \"input_data_tbl\"\r\n            },\r\n            \"node\": \"Web_input_en\",\r\n            \"attributes\": []\r\n        }],\r\n        \"exports\": [{\r\n            \"odbc\": {\r\n                \"dbRef\": \"db\",\r\n                \"table\": \"output_score_tbl\",\r\n                \"insertMode\": \"Drop\"\r\n            },\r\n            \"node\": \"Table\",\r\n            \"attributes\": []\r\n        }]\r\n    }\r\n}"
-			}
+	var data = "{\r\n    \"action\": \"BATCH_SCORE\",\r\n    \"model\": {\r\n        \"id\": \"batch1\",\r\n        \"name\": \"attract_new_en2.str\"\r\n    },\r\n    \"dbDefinitions\": {\r\n        \"db\": {\r\n            \"type\": \"PostgreSQL\",\r\n            \"host\": \"echo-01.db.elephantsql.com\",\r\n            \"port\": 5432,\r\n            \"db\": \"bgurrocc\",\r\n            \"username\": \"bgurrocc\",\r\n            \"password\": \"8n_gJj3qo5bmIB4oPQUAtfvVRW93jv0e\",\r\n            \"options\": \"\"\r\n        }\r\n    },\r\n    \"setting\": {\r\n        \"inputs\": [{\r\n            \"odbc\": {\r\n                \"dbRef\": \"db\",\r\n                \"table\": \"input_data_tbl\"\r\n            },\r\n            \"node\": \"Web_input_en\",\r\n            \"attributes\": []\r\n        }],\r\n        \"exports\": [{\r\n            \"odbc\": {\r\n                \"dbRef\": \"db\",\r\n                \"table\": \"output_score_tbl\",\r\n                \"insertMode\": \"Drop\"\r\n            },\r\n            \"node\": \"Table\",\r\n            \"attributes\": []\r\n        }]\r\n    }\r\n}";
 
-	$.ajax(settings).done(function (response) {
-	  console.log(response);
+	var xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+
+	xhr.addEventListener("readystatechange", function () {
+	  if (this.readyState === 4) {
+	    console.log(this.responseText);
+	  }
 	});
+
+	xhr.open("POST", "https://ibm-watson-ml.mybluemix.net/pm/v1/jobs/job03?accesskey=N4LnXREVJ44YgZy4MCnIJaU7NCAxoihZBHpVqS6abjaubA3inBFJ%2FeM4ZBhB2AxLHxGxQ3pIogjgEOjN0TGDTcL0h32gVzPkwMbmHXNpi%20Et7TSlwLngbf%203XI66NrGzoVlIkEPrfJsLi9tdLYT4J2hgHmZgw6ZHGfMarLtgzVM%3D");
+	xhr.setRequestHeader("content-type", "application/json;charset=UTF-8");
+	xhr.setRequestHeader("cache-control", "no-cache");
+	xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+	xhr.setRequestHeader("postman-token", "d655eabe-493c-69ec-a49f-7a8fc63859de");
+
+	xhr.send(data);
 }
