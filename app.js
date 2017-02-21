864828cb1042b9a79396e032f4dfdbf2cfc2cd0a
hjm-kawasaki <hjm.kawasaki@gmail.com>
2017-02-21 10:01:51 +0900

    test014

--- app.js
+++ app.js
@@ -9,8 +9,11 @@
 var express = require('express')
   , cors = require('cors')
   , app = express();
+var cfenv = require('cfenv');
 
 app.use(cors());
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
 
 app.use(function(request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
@@ -32,44 +35,11 @@ app.post("/", function(request, response) {
     });
 });
 
-app.listen(process.env.VCAP_APP_PORT || 8080);
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
-//app.post("/", function(request, response) {
-//    var options = {
-//        username: "replace",
-//        password: "replace"
-//    };
-//
-//    var url = "https://brsv2-6855bc66.ng.bluemix.net/DecisionService/rest" + "/vacationsRuleApp/1.0/vacationsRuleProject/json";
-//
-//    restler.postJson(url, request.body, options).on('complete', function(data) {
-//        response.send(data);
-//    });
-//});
-//
-//
-//app.get('/', function (req, res) {
-//  var data = {
-//    "bestAnimals": [
-//      "wombat",
-//      "corgi",
-//      "puffer fish",
-//      "owl",
-//      "crow"
-//    ]
-//  };
-//
-//  res.json(data);
-//});
-
-
-//// start server on the specified port and binding host
-//app.listen(appEnv.port, '0.0.0.0', function() {
-//  // print a message when the server starts listening
-//  console.log("server starting on " + appEnv.url);
-//});
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
