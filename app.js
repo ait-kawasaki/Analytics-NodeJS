773d4638eab4f72e60d1c3f90e36939a4eae601f
hjm-kawasaki <hjm.kawasaki@gmail.com>
2017-02-21 09:48:50 +0900

    test012

--- app.js
+++ app.js
@@ -6,22 +6,33 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
-var cors = require('cors');
+var app = require("express")(),
+ restler = require("restler"),
+ bodyParser = require("body-parser");
 
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+app.use(bodyParser.json());
 
-// create a new express server
-var app = express();
-app.use(cors());
 app.use(function(request, response, next) {
     response.header("Access-Control-Allow-Origin", "*");
-    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Accept");
     next();
 });
 
+
+app.post("/", function(request, response) {
+    var options = {
+        username: "resAdmin",
+        password: "replace"
+    };
+
+    var url = "https://brsv2-6855bc66.ng.bluemix.net/DecisionService/rest" +  "/vacationsRuleApp/1.0/vacationsRuleProject/json";
+
+    restler.postJson(url, request.body, options).on('complete', function(data) {
+        response.send(data);
+    });
+});
+
+app.listen(process.env.VCAP_APP_PORT || 8080);
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
